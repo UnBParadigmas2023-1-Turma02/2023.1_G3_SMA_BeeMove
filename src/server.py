032b659a5fb6c0d069cfdd80fd4a6f56153df1e6
adrian_s_lps <adriansoareslps@gmail.com>
2023-06-29 22:59:21 -0300

    Cria arquivo para iniciar servidor mesa
    Co-authored-by: Daniel <DanielViniciusAlves@users.noreply.github.com>

---
+++ src/server.py
@@ -0,0 +1,32 @@
+import mesa
+
+from src.model import Colmeia
+from src.render import render
+
+
+canvas_element = mesa.visualization.CanvasGrid(render, 100, 100, 600, 600)
+
+
+model_params = {
+    "abelhas_iniciais": mesa.visualization.Slider(
+        "População inicial de abelhas", 100, 10, 300
+    ),
+    "zangao_inicial": mesa.visualization.Slider(
+        "População inicial de zangões", 2, 1, 10
+    ),
+    "colmeia_inicial": mesa.visualization.Slider(
+        "Quantidade inicial de colmeias", 2, 1, 10
+    ),
+    "vida_adicional": mesa.visualization.Slider(
+        "Vida adicional ao se alimentar", 50, 1, 100
+    ),
+    "quantidade_defensora": mesa.visualization.Slider(
+        "Quantidade inicial de abelhas defensoras", 5, 1, 50
+    ),
+}
+
+
+server = mesa.visualization.ModularServer(
+    Colmeia, [canvas_element], "Colmeia", model_params
+)
+server.port = 8521
