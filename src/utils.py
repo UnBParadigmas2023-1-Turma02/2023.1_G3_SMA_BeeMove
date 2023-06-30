1dbf7e1449fb7daadb8bfe688ca2ad3e79188e67
klyssmannoliveira <klyssmannoliveira@gmail.com>
2023-06-29 23:08:40 -0300

    Adicionando arquivo utils.py
    Co-authored-by: Eduardo Maia <emaiarezende@gmail.com>

---
+++ src/utils.py
@@ -0,0 +1,33 @@
+from math import sqrt
+from random import randint
+
+def calculate_distance(pos1, pos2):
+    return sqrt((pos1[0] - pos2[0])**2 + (pos1[1] - pos2[1])**2)
+
+def random_pos(width, height):
+    return (
+        randint(0, width-1),
+        randint(0, height-1)
+    )
+
+def random_create_bee():
+    return randint(0, 100)
+
+# Retorna o agente que se encontra na posição atual
+# Baseado no exemplo sugarscape do mesa
+def get_item(self, agent_type):
+    for agent in self.model.grid.get_cell_list_contents([self.pos]):
+        if type(agent) is agent_type and agent != self:
+            return agent
+
+
+def get_all_queens(self):
+    for agent in self.model.grid.get_cell_list_contents([self.pos]):
+        if type(agent) is agent_type and agent != self:
+            return agent
+
+
+def get_group_color(groups, findPos):
+    for x, y, color in groups:
+        if x == findPos[0] and y == findPos[1]:
+            return color
