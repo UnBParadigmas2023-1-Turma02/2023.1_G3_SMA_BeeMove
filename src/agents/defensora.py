bdff8641159b4c6fe449fea4ee5c607b1ee338ed
06468709121 <dannysoaresdeoliveira@gmail.com>
2023-07-02 11:55:33 -0300

    criação do agente defensora
    Co-Authored-By: Bianca Sofia  <sofiabrasil44@gmail.com>

--- src/agents/defensora.py
+++ src/agents/defensora.py
@@ -1,5 +1,5 @@
 from mesa import Agent
-
+from src.agents.zangao import Zangao
 
 class Defensora(Agent):
     def __init__(self, unique_id, model):
@@ -8,3 +8,25 @@ class Defensora(Agent):
         self.vida_maxima = 120
         self.vida = self.vida_maxima
         self.poder_ataque = 10
+
+    def detecta_ameaca(self):
+        neighbors = self.model.grid.get_neighbors(self.pos, moore=True)
+        for neighbor in neighbors:
+            if isinstance(neighbor, Zangao):
+                # Verifica se o zangão pertence à mesma colmeia
+                if neighbor.colmeia == self.colmeia:
+                    continue
+                print("Ameaça detectada!")  # Adiciona o print para verificar se a função é chamada
+                return True
+        return False
+
+    def attack(self):
+        if self.detecta_ameaca():
+            neighbors = self.model.grid.get_neighbors(self.pos, moore=True)
+            for neighbor in neighbors:
+                if isinstance(neighbor, Zangao):
+                    # Verifica se o zangão pertence à mesma colmeia
+                    if neighbor.colmeia == self.colmeia:
+                        continue
+                    self.model.kill_list.append(neighbor)
+
